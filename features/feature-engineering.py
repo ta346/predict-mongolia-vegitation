c1872752c07cf1024a1daffe8718634a9c8e9e44
Natan Vidra <natanvidra@Natans-MBP.cable.rcn.com>
2022-03-28 23:56:15 -0400

    features

--- features/feature-engineering.py
+++ features/feature-engineering.py
@@ -13,25 +13,8 @@ def parseDate(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame:
     """
-    df["date"] = df["system:index"].str.split("_").str[3]
-
-    return df
-
-
-def get_datetime(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Function extracts year, month and date from date
-
-    Args:
-        df (pd.DataFrame):
-
-    Returns:
-        pd.DataFrame:
-    """
-    date_string = str(df["date"])
-    df["year"] = date_string[:4]
-    df["month"] = date_string[4:6]
-    df["day"] = date_string[6:8]
+    df["date"] = df["system:index"].str.split("_").str[-2]
+    df["date"] = pd.to_datetime(df['date'])
 
     return df
 
